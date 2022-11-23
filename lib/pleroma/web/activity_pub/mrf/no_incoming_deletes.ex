4f19ef1695d60d501b319775cce328334b2eda48
Zero <zero@cock.li>
2022-11-23 05:54:58 -0500

    Add NoIncomingDeletes MRF

---
+++ lib/pleroma/web/activity_pub/mrf/no_incoming_deletes.ex
@@ -0,0 +1,29 @@
+# Pleroma: A lightweight social networking server
+# Copyright © 2017-2022 Pleroma Authors <https://pleroma.social/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule Pleroma.Web.ActivityPub.MRF.NoIncomingDeletes do
+  @moduledoc "Reject remote deletes."
+
+  require Logger
+
+  @behaviour Pleroma.Web.ActivityPub.MRF.Policy
+  @impl true
+  def filter(%{"type" => "Delete", "actor" => actor} = object) do
+    actor_info = URI.parse(actor)
+    instance_domain = Pleroma.Config.get([Pleroma.Web.Endpoint, :url, :host])
+    if (actor_info.host == instance_domain) do
+      Logger.warn("DELETE from this instance, not rejecting: #{inspect(object)}")
+      {:ok, object}
+    else
+      Logger.warn("DELETE rejected: #{inspect(object)}")
+      {:reject, object}
+    end
+  end
+
+  @impl true
+  def filter(object), do: {:ok, object}
+
+  @impl true
+  def describe, do: {:ok, %{}}
+end
