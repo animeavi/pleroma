465086aa815a58bca29bdeb1e7225dfa8b2f3cdf
Zero <zero@cock.li>
2023-01-31 12:46:21 -0500

    ChangeReactstoLikes: only apply to Soapbox instances
    also add 🤔

--- lib/pleroma/web/activity_pub/mrf/change_react_to_like.ex
+++ lib/pleroma/web/activity_pub/mrf/change_react_to_like.ex
@@ -8,6 +8,45 @@ defmodule Pleroma.Web.ActivityPub.MRF.ChangeReactstoLikes do
   @moduledoc "Changes specified EmojiReacts into a Like"
   @behaviour Pleroma.Web.ActivityPub.MRF.Policy
 
+  defp has_soapbox_header(host) do
+    api_url = "https://" <> host <> "/api/v1/instance"
+    api_resp = HTTPoison.get!(api_url)
+
+    if api_resp.status_code == 200 do
+      with {:ok, resp_body} <- Jason.decode(api_resp.body) do
+        if resp_body["soapbox"] do
+          true
+        else
+          false
+        end
+      end
+    else
+      false
+    end
+  end
+
+  defp is_soapbox(object) do
+    known_soapbox_hosts = [
+      "poa.st",
+      "gleasonator.com",
+      "spinster.xyz",
+      "leafposter.club",
+      "chudbuds.lol",
+      "nicecrew.digital"
+    ]
+
+    skip_hosts = [""]
+    actor = object["object"]["actor"]
+    host = URI.parse(actor).host
+
+    cond do
+      is_remote(host) && Enum.member?(skip_hosts, host) -> false
+      is_remote(host) && Enum.member?(known_soapbox_hosts, host) -> true
+      is_remote(host) && has_soapbox_header(host) -> true
+      true -> false
+    end
+  end
+  
   defp is_remote(host) do
     my_host = Pleroma.Config.get([Pleroma.Web.Endpoint, :url, :host])
     my_host != host
@@ -16,14 +55,11 @@ defmodule Pleroma.Web.ActivityPub.MRF.ChangeReactstoLikes do
   @impl true
   @spec filter(any) :: {:ok, any}
   def filter(%{"type" => "EmojiReact"} = object) do
-    actor = object["actor"]
-    host = URI.parse(actor).host
-
-    if is_remote(host) do
+    if is_soapbox(object) do
       react = object["content"]
 
       # TODO: make this pull from config
-      if react in ["👍", "👎", "❤️", "😆", "😮", "😢", "😩", "😭", "🔥", "⭐"] do
+      if react in ["👍", "👎", "❤️", "😆", "😮", "😢", "😩", "😭", "🔥", "⭐", "🤔"] do
         object =
           object
           |> Map.put("type", "Like")
