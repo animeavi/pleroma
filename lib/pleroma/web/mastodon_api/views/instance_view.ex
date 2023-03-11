cc4a2dc72d26212037509726b7962bceb63acacc
foxing <foxing@noreply.akkoma>
2023-03-11 03:26:48 +0000

    Show bubble_timeline in the api if any instances are set in it, do not show if none are set

--- lib/pleroma/web/mastodon_api/views/instance_view.ex
+++ lib/pleroma/web/mastodon_api/views/instance_view.ex
@@ -66,6 +66,9 @@ defmodule Pleroma.Web.MastodonAPI.InstanceView do
       "multifetch",
       "pleroma:api/v1/notifications:include_types_filter",
       "editing",
+      if Config.get([:instance, :local_bubble], []) != [] do
+        "bubble_timeline"
+      end,
       if Config.get([:media_proxy, :enabled]) do
         "media_proxy"
       end,
