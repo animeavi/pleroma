e69661b3659567199e79834d6230e95e00e20030
FloatingGhost <hannah@coffee-and-dreams.uk>
2023-04-14 17:56:34 +0100

    Mix format

--- lib/pleroma/web/plugs/http_security_plug.ex
+++ lib/pleroma/web/plugs/http_security_plug.ex
@@ -117,11 +117,13 @@ defmodule Pleroma.Web.Plugs.HTTPSecurityPlug do
     font_src = "font-src 'self' data:"
 
     script_src = "script-src 'self' '#{nonce_tag}' "
-    script_src = if @mix_env == :dev do
-      "script-src 'self' 'unsafe-eval' 'unsafe-inline'"
-    else
-      script_src
-    end
+
+    script_src =
+      if @mix_env == :dev do
+        "script-src 'self' 'unsafe-eval' 'unsafe-inline'"
+      else
+        script_src
+      end
 
     report = if report_uri, do: ["report-uri ", report_uri, ";report-to csp-endpoint"]
     insecure = if scheme == "https", do: "upgrade-insecure-requests"
