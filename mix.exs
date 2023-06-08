cb88582fa112c06476998edf326d7469308cf6bf
Zero <zero@cock.li>
2023-06-08 00:45:15 -0400

    Silence warnings

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pleroma.Mixfile do
     [
       app: :pleroma,
       version: version("3.8.0"),
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       elixirc_options: [warnings_as_errors: warnings_as_errors()],
