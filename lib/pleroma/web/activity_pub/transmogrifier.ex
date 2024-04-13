6df504a58dfe44018a26ed35a3b7b9f7949b5859
Floatingghost <hannah@coffee-and-dreams.uk>
2024-04-13 22:25:31 +0100

    require logger

--- lib/pleroma/web/activity_pub/transmogrifier.ex
+++ lib/pleroma/web/activity_pub/transmogrifier.ex
@@ -25,6 +25,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
   import Ecto.Query
 
   require Pleroma.Constants
+  require Logger
 
   @doc """
   Modifies an incoming AP object (mastodon format) to our internal format.
@@ -160,10 +161,6 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
           object
       end
     else
-      {:quoting?, _} ->
-        object
-
-      _ ->
         object
     end
   end
