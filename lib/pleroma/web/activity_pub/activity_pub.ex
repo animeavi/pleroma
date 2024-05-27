5a374e5be307950f5d166418e28e0677fac9a53f
Floatingghost <hannah@coffee-and-dreams.uk>
2024-05-27 02:09:48 +0100

    Add extra test case for nonsense field, increase timeouts

--- lib/pleroma/web/activity_pub/activity_pub.ex
+++ lib/pleroma/web/activity_pub/activity_pub.ex
@@ -1567,7 +1567,10 @@ defmodule Pleroma.Web.ActivityPub.ActivityPub do
       data
       |> Map.get("attachment", [])
       |> normalize_attachment()
-      |> Enum.filter(fn %{"type" => t} -> t == "PropertyValue" end)
+      |> Enum.filter(fn
+        %{"type" => t} -> t == "PropertyValue"
+        _ -> false
+      end)
       |> Enum.map(fn fields -> Map.take(fields, ["name", "value"]) end)
 
     emojis =
