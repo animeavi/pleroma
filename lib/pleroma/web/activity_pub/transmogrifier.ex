efac9a383303276282c627eda9a111d7040d1ea6
Oneric <oneric@oneric.stub>
2024-11-09 16:52:12 +0100

    Completely omit id for anonymous objects
    Current AP spec demands anonymous objects to have an id value,
    but explicitly set it to JSON null. Howeveras it turns out this is
    incompatible with JSON-LD requiring `@id` to be a string and thus AP
    spec is incompatible iwth the Ativity Streams spec it is based on.
    This is an issue for (the few) AP implementers actually performing
    JSON-LD processing, like IceShrimp.NET.
    This was uncovered by IceShrimp.NET’s zotan due to our adoption of
    anonymous objects for emoj in f1018867097e6f293d8b2b5b6935f0a7ebf99bd0.
    
    The issues is being discussed by W3C, and will most likely be resolved
    via an errata redefining anonymous objects to completely omit the id
    field just like transient objects already do. See:
    https://github.com/w3c/activitypub/issues/476
    
    Fixes: https://akkoma.dev/AkkomaGang/akkoma/issues/848

--- lib/pleroma/web/activity_pub/transmogrifier.ex
+++ lib/pleroma/web/activity_pub/transmogrifier.ex
@@ -949,8 +949,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
       "icon" => %{"url" => "#{URI.encode(url)}", "type" => "Image"},
       "name" => ":" <> name <> ":",
       "type" => "Emoji",
-      "updated" => "1970-01-01T00:00:00Z",
-      "id" => nil
+      "updated" => "1970-01-01T00:00:00Z"
     }
   end
 
