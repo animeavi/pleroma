87f1276a4093a1528dcefaf01572050810ef0f0b
Oneric <oneric@oneric.stub>
2025-01-11 21:25:00 +0100

    db: drop legacy key fields in users table

--- test/pleroma/user_test.exs
+++ test/pleroma/user_test.exs
@@ -1753,7 +1753,6 @@ defmodule Pleroma.UserTest do
         bio: "eyy lmao",
         name: "qqqqqqq",
         password_hash: "pdfk2$1b3n159001",
-        keys: "RSA begin buplic key",
         avatar: %{"a" => "b"},
         tags: ["qqqqq"],
         banner: %{"a" => "b"},
